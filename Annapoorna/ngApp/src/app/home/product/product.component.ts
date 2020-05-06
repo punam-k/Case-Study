b32b04574db2e6996590f4e6a746b141933d218c
punam-k <Punam@9396>
2020-05-06 16:24:41 +0530

    Food application

---
+++ Annapoorna/ngApp/src/app/home/product/product.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../service/product.service';
+import { Router } from '@angular/router';
+import { CartService } from '../../service/cart.service';
+import { Item } from '../../model/product';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent implements OnInit {
+
+  item:Item[];
+
+  constructor(private itemService:ProductService,private router:Router,) { }
+
+  ngOnInit(): void {
+  }
+  // getItemsFromService(){
+  //   this.itemService.getItems().subscribe((response)=>{
+  //     this.item=response;
+  //     console.log(this.item);
+  
+  //   });
+  // }
+}
+
